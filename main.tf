8a2319b897ead9b9ea63fa2aaa1071ec62a8fbd4
Gabriele Lomuscio <lomuscig@amazon.fr>
2022-12-05 14:27:33 +0100

    restricting ports for influxdb node

--- main.tf
+++ main.tf
@@ -593,9 +593,16 @@ resource "aws_security_group" "influxdb_node_sg" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
